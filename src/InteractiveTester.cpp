3d05e2881973364e24c4602513c844089ebde2fe
KrugersTitan <mocelikeeng@gmail.com>
2020-08-02 18:36:39 -0400

    Created an Interactive Tester class
    The InteractiveTester has a TwoFourTree instance and provides an interface for the user to add and remove numbers from the tree to watch it grow or shrink.
    For now the only completed implementation is simple addition, but the prototypes for removing were created.

--- src/InteractiveTester.cpp
+++ src/InteractiveTester.cpp
@@ -9,15 +9,121 @@
  */
 
 #include <InteractiveTester.hpp>
+#include <Utility.hpp>
+
+#include <tftree.hpp>
+
+#include <string>
+#include <sstream>
 
 namespace tft {
 
-InteractiveTester::InteractiveTester() {
-	// TODO Auto-generated constructor stub
+void InteractiveTester::add(const std::string& line){
+	std::stringstream ss(line);
 
+	std::string str;
+	ss >> str;
+
+	if (str != "add")
+		return;
+
+	ss >> str;
+	if (str == "random") {
+		std::cout << "unhandled add random";
+		return;
+	}
+
+	try {
+		int num = std::stoi(str);
+		ss >> str;
+
+		if (str == "to") {
+			ss >> str;
+			int start = num;
+			int end = std::stoi(str);
+
+			for (int i = start; i < end; i++)
+				tree_.insert(int(i));
+			return;
+		} else {
+			tree_.insert(int(num));
+			tree_.insert(std::stoi(str));
+
+			while (ss >> num)
+				tree_.insert(int(num));
+		}
+	} catch (std::exception& e) {
+		std::cout << "Expected number\n";
+	}
 }
 
-InteractiveTester::~InteractiveTester() {
-	// TODO Auto-generated destructor stub
+void InteractiveTester::erase(const std::string& line){
+	std::cout << "Entering function: " << __FUNCTION__ << " with line " << line <<std::endl;
 }
+void InteractiveTester::add_multi(const std::string& line){
+	std::cout << "Entering function: " << __FUNCTION__ << " with line " << line <<std::endl;
+}
+void InteractiveTester::clear(const std::string& line) {
+	std::cout << "Entering function: " << __FUNCTION__ << " with line " << line << std::endl;
+}
+
+void InteractiveTester::resetTerminal() {
+	std::cout << "\033[2J"; // clear terminal
+	std::cout << "\033[1;1H"; // position cursor at 1:1
+}
+
+void InteractiveTester::run() {
+	resetTerminal();
+
+	while( true ) {
+
+		int instruction_width = 23;
+		std::cout << tree_ << std::endl;
+		std::cout << "On the left are commands with their effects on the tree described to the right:" << std::endl
+				<< std::left
+				<< std::setw(instruction_width) << "  add 1 2 3 4" 	<< "adds 1,2,3, and 4" << std::endl
+				<< std::setw(instruction_width) << "  add 0 to 5" 	<< "adds [0, 5)" << std::endl
+				<< std::setw(instruction_width) << "  add random 5" 	<< "adds 5 random numbers " << std::endl
+				<< std::setw(instruction_width) << "  remove 1 2 3" 	<< "removes 1,2 and 3" << std::endl
+				<< std::setw(instruction_width) << "  remove 0 to 5" << "removes [0, 5)" << std::endl
+				<< std::setw(instruction_width) << "  clear" 		<< "removes all values from the tree, resetting it" << std::endl
+				<< std::setw(instruction_width) << "  q" 			<< "quit" << std::endl;
+
+		std::cout << "Please enter a properly formatted instruction now: ";
+		std::string line;
+		std::getline(std::cin, line);
+
+		trimSpaces(line);
+
+		if (!std::cin) {
+			if (std::cin.bad()) {
+				std::cerr << "I/O error\n";
+				return;
+			} else if (std::cin.eof()) {
+				std::cerr << "Unexpected end of file\n";
+				return;
+			} else if (std::cin.fail()) {
+				std::cerr << "Unexpected error\n";
+				std::cin.clear();
+				return;
+
+			} else {
+				std::cerr << "Unknown error when reading\n";
+				return;
+			}
+		}
+
+		if (startsWith(line, "add")) {
+			add(line);
+		} else if (startsWith(line, "erase")) {
+			erase(line);
+		} else if (startsWith(line, "clear")) {
+			clear(line);
+		} else if (line == "q") {
+			return;
+		}
+		resetTerminal();
+	}
+}
+
 } /* namespace tft */
--- src/InteractiveTester.hpp
+++ src/InteractiveTester.hpp
@@ -11,12 +11,30 @@
 #ifndef SRC_INTERACTIVETESTER_HPP_
 #define SRC_INTERACTIVETESTER_HPP_
 
+#include <map>
+#include <string>
+#include <functional>
+
+#include "tftree.hpp"
+
 namespace tft {
 
 class InteractiveTester {
 public:
-	InteractiveTester();
-	virtual ~InteractiveTester();
+	InteractiveTester() = default;
+	virtual ~InteractiveTester() = default;
+
+	void run();
+
+private:
+	void resetTerminal();
+
+	void add(const std::string& line);
+	void erase(const std::string& line);
+	void add_multi(const std::string& line);
+	void clear(const std::string& line);
+
+	TwoFourTree<long> tree_;
 };
 
 } /* namespace tft */
