332ac95a61011f3ee3abdef82873634777f838d4
KrugersTitan <mocelikeeng@gmail.com>
2020-10-04 01:03:11 -0400

    Updated controller to switch between interactive and benchmark

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -12,22 +12,48 @@
 
 #include "tftree.hpp"
 #include "InteractiveTester.hpp"
+#include "Benchmarker.hpp"
+#include "Utility.hpp"
 
 #include <iostream>
 
 namespace tft {
 
 int Controller::start(int argc, char **argv) {
-	std::cout << "Controller\n";
-	std::cout << "params: " << argc << ' ';
-	for (int i =0; i < argc; i++)
-		std::cout << argv[i] << ' ';
-	std::cout << std::endl;
-
+	Benchmarker bm;
 	InteractiveTester ui;
-	ui.run();
 
-	return 0;
+	while (true) {
+		clearTerminal();
+
+		std::cout << "b or benchmark to enter benchmark mode\n"
+					 "i or interactive to enter interactive mode\n"
+					 "h or help to read a help message\n"
+					 "q or quit to exit\n";
+
+		std::string input;
+		std::getline(std::cin, input);
+		if (!std::cin) {
+			std::cout << "IO error. Exiting\n";
+			return EXIT_FAILURE;
+		}
+
+		trimSpaces(input);
+
+		if (input == "b" || input == "benchmark") {
+			bm.run();
+		} else if (input == "i" || input == "interactive") {
+			ui.run();
+		} else if (input == "h" || input == "help") {
+			std::cout << "TODO: help message\n";
+		} else if (input == "q" || input == "quit") {
+			std::cout << "Bye!\n";
+			return EXIT_SUCCESS;
+		}
+
+		if (!std::cin)
+			return EXIT_FAILURE;
+	}
 }
 
 } /* namespace tft */
