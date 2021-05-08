bc91fbf69ca7a5e19152aabb64e02e36f2e466d5
Muhammed O. Celik <mocelikeeng@gmail.com>
2021-05-08 01:05:02 -0400

    Created basic benchmark code to compare the set and the tree

--- src/Benchmarker.cpp
+++ src/Benchmarker.cpp
@@ -12,39 +12,209 @@
 
 #include <tftree.hpp>
 #include <DataGenerator.hpp>
+#include <Utility.hpp>
 
 #include <chrono>
 #include <iostream>
 #include <iomanip>
-#include <limits>
-#include <set>
+#include <vector>
+#include <random>
+
+namespace {
+/**
+ * Calls the callable object and returns the duration it took in microseconds
+ */
+template <typename Callable>
+std::chrono::microseconds measure(Callable c ) {
+	auto begin = std::chrono::steady_clock::now();
+	c();
+	auto end = std::chrono::steady_clock::now();
+
+	return std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
+}
+
+}
 
 namespace tft {
 
 void Benchmarker::run() {
+	clearTerminal();
 	std::cout << "Benchmark code - in progress\n";
 
-	TwoFourTree<int> tree;
-	std::set<int> set;
+	while (true) {
+		printSettings();
 
-	std::cout << "Size?\n";
-	int size;
-	std::cin >> size;
+		std::cout << "Please choose one of the following options:\n"
+				"d - change the random distribution type\n"
+				"s - change the size of the benchmark (number of elements to insert/erase)\n"
+				"r - change the range that elements can be selected from\n"
+				"i - benchmark insertion\n"
+				"e - benchmark erasing\n"
+				"c - clear the tree and set\n"
+				"q - quit\n";
 
-	auto test_values = generateRandom(size, std::numeric_limits<int>::min(), std::numeric_limits<int>::max(), EDistribution::BINOMIAL_50);
+		std::string input;
+		std::getline(std::cin, input);
+		if (!std::cin) {
+			std::cerr << "IO Error\n";
+			return;
+		}
 
-	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-	for (const auto& it : test_values)
-		set.insert(int(it));
-	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-	std::cout << std::setw(15) << "set took: " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << " us\n";
+		clearTerminal();
 
-	begin = std::chrono::steady_clock::now();
-	for (const auto& it : test_values)
-		tree.insert(int(it));
-	end = std::chrono::steady_clock::now();
-	std::cout << std::setw(15) << "tree took: " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << " us\n";
+		if (input == "d") {
+			setDistribution();
+		} else if (input == "s") {
+			setSize();
+		} else if (input == "r") {
+			setRange();
+		} else if (input == "i") {
+			insert();
+		} else if (input == "e") {
+			erase();
+		} else if (input == "c") {
+			clear();
+		} else if (input == "q") {
+			return;
+		} else {
+			printTimes("No op", measure([](){}), measure([](){}));
+		}
 
+	}
 }
+
+std::vector<int> Benchmarker::generateRandom() const{
+	return tft::generateRandom(bm_size_, 0 /* min value */, range_, distrib_);
+}
+
+void Benchmarker::printTimes(const std::string& operation, const std::chrono::microseconds& std_time, const std::chrono::microseconds& tft_time) const {
+	std::cout << operation << " run times:\n"
+			"Set         -  [" << std_time.count() << " us]\n"
+			"TwoFourTree -  [" << tft_time.count() << " us]\n\n";
+}
+
+void Benchmarker::printSettings() const {
+	std::cout << "Current settings are:\n";
+	std::cout << "Distribution: " << distrib_ <<std::endl;
+	std::cout << "Number of elements: " << bm_size_ << std::endl;
+	std::cout << "Range (max value): " << range_ << std::endl;
+	std::cout << "Current tree/set size: " << tree_.size() << std::endl;
+
+	std::cout << std::endl << std::endl;
+}
+
+void Benchmarker::setDistribution(){
+
+	std::cout << "Distributions:\n"
+			"uni\t Uniform distribution\n"
+			"b20\t Binomial Distribution with p = 0.20\n"
+			"b50\t Binomial Distribution with p = 0.50\n"
+			"b80\t Binomial Distribution with p = 0.80\n";
+
+	std::string input;
+	std::getline(std::cin, input);
+	if (!std::cin)
+		return;
+
+	if (input ==  "uni")
+		distrib_ = EDistribution::UNIFORM;
+	else if (input == "b20")
+		distrib_ = EDistribution::BINOMIAL_20;
+	else if (input == "b50")
+		distrib_ = EDistribution::BINOMIAL_50;
+	else if (input == "b80")
+		distrib_ = EDistribution::BINOMIAL_80;
+}
+
+void Benchmarker::setSize(){
+	std::string input;
+	std::cout << "How many elements should be used in the benchmark? (A higher number will take longer to run): ";
+	while (true) {
+		std::getline(std::cin, input);
+		try {
+			bm_size_ = std::stol(input);
+			return;
+		} catch (std::exception& e) {
+			std::cout << e.what() << "\nPlease try again.\n";
+		}
+	}
+}
+
+void Benchmarker::setRange(){
+	std::string input;
+	std::cout << "What should the maximum value be? Recommend to set to 5x the benchmark size (" << 5*bm_size_ << "): ";
+	while (true) {
+		std::getline(std::cin, input);
+		try {
+			range_ = std::stoi(input);
+			return;
+		} catch (std::exception& e) {
+			std::cout << e.what() << "\nPlease try again.\n";
+		}
+	}
+}
+
+void Benchmarker::insert(){
+	auto test_values = generateRandom();
+
+	auto set_size = set_.size();
+	auto std_time = measure([this, &test_values]() {
+		for (const auto& it : test_values)
+			this->set_.insert(int(it));
+	});
+	if (set_size == set_.size())
+		std::cerr << "Set size did not change after insert\n";
+
+	auto tft_time = measure([this, &test_values]() {
+		for (const auto &it : test_values)
+			tree_.insert(int(it));
+	});
+
+	printTimes("Inserting " + std::to_string(bm_size_) + " elements", std_time, tft_time);
+}
+
+// Generate the list of numbers to be erased by iteratin  over the set and choosing numbers at random
+// Then go through the tree and set while removing the chosen numbers
+void Benchmarker::erase(){
+	auto eraseSize = bm_size_ > set_.size() ? set_.size() : bm_size_;
+
+	auto gen = generator();
+	std::bernoulli_distribution bernoulli(static_cast<double>(eraseSize) / set_.size());
+	std::vector<int> toErase;
+	for (const auto& it : set_)
+		if (bernoulli(gen))
+			toErase.emplace(toErase.end(), it);
+
+	// toErase now has an expected size of eraseSize
+	// there will likely be a small deviation from eraseSize but that is acceptable
+
+	// Since the set_ might already be cached because of recent access, we will first time
+	// the tree.
+
+	auto tft_time = measure([this, &toErase]() {
+		for (const auto &it : toErase)
+			tree_.erase(it);
+	});
+
+	auto std_time = measure([this, &toErase]() {
+		for (const auto &it : toErase)
+			set_.erase(it);
+	});
+
+	printTimes("Erasing " + std::to_string(toErase.size()) + " elements", std_time, tft_time);
+}
+
+void Benchmarker::clear(){
+	auto std_time = measure([this]() {
+		set_.clear();
+	});
+
+	auto tft_time = measure([this]() {
+		tree_.clear();
+	});
+
+	printTimes("Clearing", std_time, tft_time);
+}
+
 
 } /* namespace tft */
--- src/Benchmarker.hpp
+++ src/Benchmarker.hpp
@@ -11,6 +11,14 @@
 #ifndef SRC_BENCHMARKER_HPP_
 #define SRC_BENCHMARKER_HPP_
 
+#include "tftree.hpp"
+#include "DataGenerator.hpp"
+
+#include <unordered_set>
+#include <set>
+#include <chrono>
+#include <string>
+
 namespace tft {
 
 class Benchmarker {
@@ -19,6 +27,26 @@ public:
 	virtual ~Benchmarker() = default;
 
 	void run();
+
+private:
+	std::vector<int> generateRandom() const;
+	void printTimes(const std::string &operation,
+			const std::chrono::microseconds &std_time,
+			const std::chrono::microseconds &tft_time) const;
+	void printSettings() const;
+
+	void setDistribution();
+	void setSize();
+	void setRange();
+	void insert();
+	void erase();
+	void clear();
+
+	TwoFourTree<int> tree_;
+	std::set<int> set_;
+	EDistribution distrib_ { EDistribution::UNIFORM };
+	TwoFourTree<int>::size_type bm_size_ { 10000 };
+	int range_ { 50000 };
 };
 
 } /* namespace tft */
